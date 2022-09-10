b4eeb19431467dcff7b603959a9c861bf527c33a
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2022-09-10 07:05:36 +0530

    continued work on the IWP DA1
    it's tough and nothing seems to work well with each other , there is always something not working as desized

---
+++ lab6/index.js
@@ -0,0 +1,31 @@
+function validation() {
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let businessOS = document.getElementById("businessOS").value;
+    let freqby = document.getElementById("freqby").value;
+    let lreqby = document.getElementById("lreqby").value;
+    let emailR = document.getElementById("emailR").value;
+    let phone = document.getElementById("phone").value;
+    let fholders = document.getElementById("fholders").value;
+    let lholders = document.getElementById("lholders").value;
+    let add1 = document.getElementById("add1").value;
+    let add2 = document.getElementById("add2").value;
+    let city = document.getElementById("city").value;
+    let state = document.getElementById("state").value;
+    let postalcode = document.getElementById("Postalcode").value;
+    let country = document.getElementById("country").value;
+    let emailC = document.getElementById("emailC").value;
+    let project = document.getElementById("project").value;
+    let jobdes = document.getElementById("jobdes").value;
+
+    if (fname == "" || lname == "" || businessOS == "" || freqby == "" || lreqby =="" || emailR=="" || phone=="" || fholders == "" || lholders =="" || add1=="" || add2=="" || city=="" || state=="" || postalcode=="" || country=="" || emailC=="" || project=="" || jobdes=="") {
+        alert("Please fill in all the fields !");
+        return false;
+    }
+
+
+    var emailpattern = /[A-z]/
+    if (emailC=="") {
+        alert("Email blank");
+    }
+}
